61702d424c29f07888d103417ac5526a51636380
Bartłomiej Boczek <goterix1@gmail.com>
2018-07-22 22:47:14 +0200

    Alias to .dotfiles repo

--- .dotfiles/repo_aliases.sh
+++ .dotfiles/repo_aliases.sh
@@ -1,2 +1,3 @@
 alias diploma_repo="open -a Firefox 'https://github.com/boczekbartek/SpeakerRecognition'"
 alias twitter_repo="open -a Firefox 'https://github.com/boczekbartek/TwitterSentimentAnalyser-NN'"
+alias dot_repo="open -a Firefox 'https://github.com/boczekbartek/.dotfiles'"
