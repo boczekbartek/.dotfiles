419ef47519c3caf683bbd90a275c6c476d7b8655
Bartłomiej Boczek <goterix1@gmail.com>
2018-08-25 16:31:07 +0200

    Changed browser in github aliases from Firefox to Safari

--- .dotfiles/repo_aliases.sh
+++ .dotfiles/repo_aliases.sh
@@ -1,3 +1,3 @@
-alias diploma_repo="open -a Firefox 'https://github.com/boczekbartek/SpeakerRecognition'"
-alias twitter_repo="open -a Firefox 'https://github.com/boczekbartek/TwitterSentimentAnalyser-NN'"
-alias dot_repo="open -a Firefox 'https://github.com/boczekbartek/.dotfiles'"
+alias diploma_repo="open -a Safari 'https://github.com/boczekbartek/SpeakerRecognition'"
+alias twitter_repo="open -a Safari 'https://github.com/boczekbartek/TwitterSentimentAnalyser-NN'"
+alias dot_repo="open -a Safari 'https://github.com/boczekbartek/.dotfiles'"
