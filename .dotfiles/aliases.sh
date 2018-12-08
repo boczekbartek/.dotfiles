9233563bd9b548d73a7be442cd7422b60cb4288c
Bartłomiej Boczek <bartek1boczek@gmail.com>
2018-12-08 17:03:46 +0100

    Function for showing hidden file

--- .dotfiles/aliases.sh
+++ .dotfiles/aliases.sh
@@ -7,3 +7,12 @@ alias p=pbpaste
 
 # commonly mistaken commands fixups
 alias sl=ls
+
+# print buw
+buw () {
+  sudo cat ~/.buw_wifi;
+  echo "Press ENTER to clear..."
+  read
+  clear;
+}
+export -f buw >/dev/null
